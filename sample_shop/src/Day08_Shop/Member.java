9c6edbda7c7a4061c403a4d71cbb5376c3e86bf7
pc <pc@DESKTOP-PE3MK3B>
2021-10-27 19:05:21 +0900

    211027_수정 파일정리

---
+++ sample_shop/src/Day08_Shop/Member.java
@@ -0,0 +1,5 @@
+package Day08_Shop;
+
+public class Member {
+
+}
---
+++ sample_shop/src/Shop3/iShop.java
@@ -0,0 +1,12 @@
+package Shop3;
+
+public interface iShop {
+	void setTitle(String title); // 앞에 public abstract이 생략되었음
+
+	void genUser();
+
+	void genProduct();
+
+	void start();
+
+}
