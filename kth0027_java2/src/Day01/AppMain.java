837b57b91fe86efc439a17a7a1087342e1bcfc9c
505 <505@DESKTOP-ILGB72L>
2021-10-28 13:38:09 +0900

    211028

--- kth0027_java2/src/Day01/AppMain.java
+++ kth0027_java2/src/Day01/AppMain.java
@@ -1,21 +1,23 @@
 package Day01;
 
 import javafx.application.Application;
+// import : javafx
+
 import javafx.stage.Stage;
 
 public class AppMain extends Application {
-					// 1. Application : javafx 에게 상속받기
-	
+	// 1. Application : javafx 에게 상속받기
+
 	// 2. 오버라이딩 : start 메소드
 	@Override
-	public void start (Stage primaryStage) throws Exception {
-						// 3. Stage 이름 정하기
-			primaryStage.show();
-			// 4. 해당 stage 실행
+	public void start(Stage 스테이지) throws Exception {
+		// 3. Stage 이름 정하기
+		스테이지.show();
+		// 4. 해당 stage 실행
 	}
-	
+
 	public static void main(String[] args) {
-		launch(args); // 5. Start 메소드 호출
+		launch(args);
 	}
 
 }
--- kth0027_java2/src/module-info.java
+++ kth0027_java2/src/module-info.java
@@ -1,5 +1,7 @@
-module java2 {
+module kth0027_java2 {
 	requires javafx.controls;
+	requires javafx.fxml; // ¼±¾ð
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens Day01 to javafx.graphics, javafx.fxml;
 }
